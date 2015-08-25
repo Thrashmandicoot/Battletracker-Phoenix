e8dbd8befa2b18e1f1db4d60b0e1b65629082186
Ashton Alexander <ashton.alexander@gmail.com>
2015-08-24 17:53:40 -0700

    file order fix

---
+++ deps/comeonin/mix.exs
@@ -0,0 +1,135 @@
+defmodule Mix.Tasks.Compile.Comeonin do
+  @shortdoc "Compiles Comeonin"
+
+  def run(_) do
+    File.mkdir("priv")
+    {exec, args} = case :os.type do
+      {:win32, _} ->
+        {"nmake", ["/F", "Makefile.win", "priv\\bcrypt_nif.dll"]}
+      {:unix, :freebsd} ->
+        {"gmake", ["priv/bcrypt_nif.so"]}
+      {:unix, :openbsd} ->
+        {"gmake", ["priv/bcrypt_nif.so"]}
+      _ ->
+        {"make", ["priv/bcrypt_nif.so"]}
+    end
+
+    if System.find_executable(exec) do
+      build(exec, args)
+    else
+      nocompiler_error(exec)
+    end
+  end
+
+  def build(exec, args) do
+    {result, error_code} = System.cmd(exec, args, stderr_to_stdout: true)
+    if error_code != 0, do: build_error(exec), else: IO.binwrite result
+  end
+
+  defp nocompiler_error("nmake") do
+    raise Mix.Error, message: nocompiler_message("nmake") <> windows_message
+  end
+  defp nocompiler_error(exec) do
+    raise Mix.Error, message: nocompiler_message(exec) <> nix_message
+  end
+
+  defp build_error("nmake") do
+    raise Mix.Error, message: build_message <> windows_message
+  end
+  defp build_error(_) do
+    raise Mix.Error, message: build_message <> nix_message
+  end
+
+  defp nocompiler_message(exec) do
+    """
+    Could not find the program `#{exec}`.
+
+    You will need to install the C compiler `#{exec}` to be able to build
+    Comeonin.
+
+    """
+  end
+
+  defp build_message do
+    """
+    Could not compile Comeonin.
+
+    Please make sure that you are using Erlang / OTP version 17.0 or later
+    and that you have a C compiler installed.
+
+    """
+  end
+
+  defp windows_message do
+    """
+    One option is to install a recent version of Visual Studio (the
+    free Community edition will be enough for this task). Then try running
+    `mix deps.compile comeonin` from the `Developer Command Prompt`. If
+    you are using 64-bit erlang, you might need to run the command
+    `vcvarsall.bat amd64` before running `mix deps.compile`. Further
+    information can be found at
+    (https://msdn.microsoft.com/en-us/library/x4d2c09s.aspx).
+
+    """
+  end
+
+  defp nix_message do
+    """
+    Please follow the directions below for the operating system you are
+    using:
+
+    Mac OS X: You need to have gcc and make installed. Try running the
+    commands `gcc --version` and / or `make --version`. If these programs
+    are not installed, you will be prompted to install them.
+
+    Linux: You need to have gcc and make installed. If you are using
+    Ubuntu or any other Debian-based system, install the packages
+    `build-essential`. Also install `erlang-dev` package if not
+    included in your Erlang/OTP version.
+
+    """
+  end
+end
+
+defmodule Comeonin.Mixfile do
+  use Mix.Project
+
+  @description """
+  Password hashing (bcrypt, pbkdf2_sha512) library for Elixir.
+  """
+
+  def project do
+    [
+      app: :comeonin,
+      version: "1.1.2",
+      elixir: "~> 1.0",
+      name: "Comeonin",
+      description: @description,
+      package: package,
+      source_url: "https://github.com/elixircnx/comeonin",
+      compilers: [:comeonin, :elixir, :app],
+      deps: deps
+    ]
+  end
+
+  def application do
+    [applications: [:crypto, :logger]]
+  end
+
+  defp deps do
+    [
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc,  "~> 0.7", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "c_src", "mix.exs", "Makefile*", "README.md", "LICENSE"],
+      contributors: ["David Whitlock", "Ben Sharman", "Jason M Barnes"],
+      licenses: ["BSD"],
+      links: %{"GitHub" => "https://github.com/elixircnx/comeonin",
+        "Docs" => "http://hexdocs.pm/comeonin"}
+    ]
+  end
+end
