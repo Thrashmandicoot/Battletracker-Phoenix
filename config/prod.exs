00b72bef9826b8df39265d424e380e71fe181c97
Ashton Alexander <ashton.alexander@gmail.com>
2015-08-24 18:54:04 -0700

    adds another heroku appname change

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :battletracker, Battletracker.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "battletracker-phoenix.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "battletracker-phoenix.heroku.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
